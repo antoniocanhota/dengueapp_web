3b91948ccdc8f320074c64dce043c79ad5d93ac8
antoniocanhota <antoniocanhota@gmail.com>
2012-12-15 17:54:03 -0200

    Correcao de sintaxe

--- app/models/denuncia.rb
+++ app/models/denuncia.rb
@@ -12,8 +12,7 @@ class Denuncia < ActiveRecord::Base
   belongs_to :denunciante
   has_attached_file :foto,
     :storage => :dropbox,
-    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
-    :dropbox_options => {...}
+    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml"
   
   accepts_nested_attributes_for :denunciante
   accepts_nested_attributes_for :dispositivo
