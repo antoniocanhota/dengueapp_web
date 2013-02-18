aef066b2d6cba2ce1f3e4519781cf06810ae12f2
antoniocanhota <antoniocanhota@gmail.com>
2013-02-17 22:09:52 -0300

    Refatorações de mapa

--- app/helpers/denuncias_helper.rb
+++ app/helpers/denuncias_helper.rb
@@ -2,27 +2,6 @@
 # encoding: utf-8
 
 module DenunciasHelper
-  
-  def mapa_do_grande_rio(marcadores)
-    gmaps("map_options" => {
-        :center_latitude => "-22.890209",
-        :center_longitude => "-43.296562", 
-        :type => "HYBRID", 
-        :zoom => 11,
-        :auto_adjust => false
-      },
-      :markers => {"data" => marcadores}
-    )
-  end 
-  
-  def mapa_pontual(marcadores)
-    gmaps("map_options" => {
-        :type => "HYBRID", 
-        :zoom => 11, 
-      },
-      :markers => {"data" => marcadores}
-    )
-  end
 
   def badge_de_situacao(situacao_codificada)
     case situacao_codificada
