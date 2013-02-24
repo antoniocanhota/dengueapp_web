d0070d6f28c0a27ed18070fed6dacaa634e4e20b
antoniocanhota <antoniocanhota@gmail.com>
2013-02-24 20:09:21 -0300

    Corrção de migração

--- db/migrate/20130201171704_drop_tabelas_de_perfis.rb
+++ db/migrate/20130201171704_drop_tabelas_de_perfis.rb
@@ -1,30 +1,30 @@
 class DropTabelasDePerfis < ActiveRecord::Migration
   def up
-    #cria a coluna situacao_denunciante e migra os dados da tabela Denunciante e aponta as novas chaves
-    add_column :usuarios, :denunciante_situacao, :string, :limit => 4
-    add_column :dispositivos, :usuario_id, :integer
-    Denunciante.find_each do |de|
-      if de.usuario
-        de.usuario.update_attribute(:denunciante_situacao,de.situacao)
-        de.dispositivos.find_each do |di|
-          di.update_attribute(:usuario_id,de.usuario_id)
-        end
-      end
-    end
-    drop_table :denunciantes
-    #cria as colunas moderador e administrador
-    add_column :usuarios, :moderador, :boolean
-    add_column :usuarios, :administrador, :boolean
-    Operador.find_each do |o|
-      if o.situacao != Operador::INATIVO
-        if o.tipo == Operador::ADMINISTRADOR
-          o.usuario.update_attribute(:administrador,true)
-        elsif o.tipo == Operador::OPERADOR
-          o.usuario.update_attribute(:moderador,true)
-        end
-      end
-    end
-    drop_table :operadores
+    ##cria a coluna situacao_denunciante e migra os dados da tabela Denunciante e aponta as novas chaves
+    #add_column :usuarios, :denunciante_situacao, :string, :limit => 4
+    #add_column :dispositivos, :usuario_id, :integer
+    #Denunciante.find_each do |de|
+    #  if de.usuario
+    #    de.usuario.update_attribute(:denunciante_situacao,de.situacao)
+    #    de.dispositivos.find_each do |di|
+    #      di.update_attribute(:usuario_id,de.usuario_id)
+    #    end
+    #  end
+    #end
+    #drop_table :denunciantes
+    ##cria as colunas moderador e administrador
+    #add_column :usuarios, :moderador, :boolean
+    #add_column :usuarios, :administrador, :boolean
+    #Operador.find_each do |o|
+    #  if o.situacao != Operador::INATIVO
+    #    if o.tipo == Operador::ADMINISTRADOR
+    #      o.usuario.update_attribute(:administrador,true)
+    #    elsif o.tipo == Operador::OPERADOR
+    #      o.usuario.update_attribute(:moderador,true)
+    #    end
+    #  end
+    #end
+    #drop_table :operadores
   end
 
   def down
