752856e0fb8b48c2240d66129f23307a7d4b5b9b
antoniocanhota <antoniocanhota@gmail.com>
2013-07-08 00:26:11 -0300

    Correção de bugs no cadastro de usuários

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,8 @@ Dengueapp::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
+  config.action_mailer.default_url_options = { :host => 'dengue.herokuapp.com' }
+
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
